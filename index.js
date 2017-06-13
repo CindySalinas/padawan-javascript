6a11dc0e008d5b497ddf07df54df8b527a106519
Cindy Salinas <cindysalinas15@gmail.com>
2017-06-13 09:44:53 -0400

    return mail data

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 'use strict';
+
 const logger = require('winston');
 const mailData = require('./static/mails');
 const firstType = require('./parsers/firstType');
@@ -10,24 +11,24 @@ const type1 = new RegExp("^\\(?\\w[A-Z0-9]{1,}\\)? ?-? ?#? \\(?[0-9]{1,}\\)?");
 const type2 = new RegExp("[\\(\\)]");
 const type3 = new RegExp("^\\(?\\w[a-z0-9\\-]{1,}\\)? ?-?");
 
-let mailGroups = {
-  type1: [],
-  type2: [],
-  type3: []
-};
-
 // separate mails by type
 const createMailGroup = (mails) => {
+  let mailGroups = {
+    type1: [],
+    type2: [],
+    type3: []
+  };
   mails.forEach(val => {
     if (type1.test(val.data['Subject'])) mailGroups.type1.push(val);
     if (type2.test(val.data['Subject'])) mailGroups.type2.push(val);
     if (type3.test(val.data['Subject'])) mailGroups.type3.push(val);
   });
+  return mailGroups;
 };
 
 // init to parse all emails
 const init = () => {
-  createMailGroup(mailData);
+  let mailGroups = createMailGroup(mailData);
   logger.info(`Data for mails that math first type`);
   firstType(mailGroups.type1).forEach((e, i) => console.log(`Mail #${i+1}:`, e));
   console.log('--------------------------------------');
